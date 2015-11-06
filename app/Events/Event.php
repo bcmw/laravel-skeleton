a710e903b38cbe814279c6e66d5c3701868bf2fd
Valeriy Trubachev <vtrubachev@bluechipww.com>
2015-11-06 14:37:59 -0600

    add event and job classes back in that extend the helper base clases

---
+++ app/Events/Event.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Events;
+
+use Bluechip\Laravel\Support\BaseEvent;
+
+abstract class Event extends BaseEvent
+{
+    //
+}
---
+++ app/Jobs/Job.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Jobs;
+
+use Bluechip\Laravel\Support\BaseJob;
+
+abstract class Job extends BaseJob
+{
+    //
+}
